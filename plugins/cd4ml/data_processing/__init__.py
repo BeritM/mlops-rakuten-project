00b771a61555c977bba9911536ac5b5b35174dbe
Anomalisaa <95170044+Anomalisaa@users.noreply.github.com>
2025-03-23 08:27:46 +0100

    in folder data_preprocessing renamed old py documents to template_* and added 4 new py-docs for the preprocessing Jupyter NBs

--- plugins/cd4ml/data_processing/__init__.py
+++ plugins/cd4ml/data_processing/__init__.py
@@ -1,6 +1,12 @@
-from cd4ml.data_processing.ingest_data import ingest_data
-from cd4ml.data_processing.ingest_data import get_data
-from cd4ml.data_processing.split_train_test import get_train_test_split, split_train_test
-from cd4ml.data_processing.transform_data import get_transformed_data, transform_data
-from cd4ml.data_processing.validate_data import validate_data
-from cd4ml.data_processing.track_data import track_data
+# Description: This file is used to import all the functions from the data_processing folder.
+
+## this piece of code is from the template and will be ignored: 
+# from cd4ml.data_processing.ingest_data import ingest_data
+# from cd4ml.data_processing.ingest_data import get_data
+# from cd4ml.data_processing.split_train_test import get_train_test_split, split_train_test
+# from cd4ml.data_processing.transform_data import get_transformed_data, transform_data
+# from cd4ml.data_processing.validate_data import validate_data
+# from cd4ml.data_processing.track_data import track_data
+
+
+## this is the new code applied to RAKUTEN use case:
