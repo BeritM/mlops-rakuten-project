097ce224187b22cd6ddf47faa532e348e85581bf
Berit <berit.schmidt.meinert@gmail.com>
2025-04-01 12:46:32 +0200

    Add model-validation py scripts

---
+++ plugins/cd4ml/model_validation/model_validation.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import SGDClassifier
+from sklearn.metrics import classification_report, f1_score, accuracy_score
+from sklearn.utils.class_weight import compute_class_weight
+import pickle
+import joblib
+from sklearn.base import BaseEstimator
+
+def load_model(output_dir: str, model_file: str) -> BaseEstimator:
+    model_path = f"{output_dir}/{model_file}"
+    model = joblib.load(model_path)
+
+    return model
+
+def prediction_and_metrics(X_validate_tfidf: pd.DataFrame, y_validate: pd.DataFrame, model: BaseEstimator):
+    y_pred_validate = model.predict(X_validate_tfidf)
+    val_accuracy = accuracy_score(y_validate, y_pred_validate)
+    val_f1 = f1_score(y_validate, y_pred_validate, average='weighted')
+    classification_rep = classification_report(y_validate, y_pred_validate)
+
+    return val_accuracy, val_f1, classification_rep
+
+def save_txt_file(output_dir: str, file_name: str, content: str):
+    report_path = f"{output_dir}/{file_name}.txt"
+    with open(report_path, "w") as f:
+        f.write(content)
---
+++ plugins/cd4ml/model_validation/run_model_validation.py
@@ -0,0 +1,37 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../model_training")))
+
+from model_validation import load_model, prediction_and_metrics, save_txt_file
+from model_training import load_train_data
+
+
+def main():
+
+    # Set paths for X and y data and output model folder
+
+    input_dir = "../../../data/processed"
+    output_dir = "../../../models"
+    X_validate_tfidf_path = f"{input_dir}/X_validate_tfidf.pkl"
+    y_validate_path = f"{input_dir}/y_validate.pkl"
+    model_file = "sgd_text_model.pkl"
+
+    model = load_model(output_dir, model_file)
+
+    X_validate_tfidf, y_validate = load_train_data(X_validate_tfidf_path, y_validate_path)
+
+    val_acc, val_f1, classification_repo = prediction_and_metrics(X_validate_tfidf, y_validate, model)
+
+    print("Prediction finished. Results:")
+    print(f"Validation accuracy: {val_acc}")
+    print(f"Validation F1: {val_f1}")
+    print(f"classification report: \n {classification_repo}")
+
+    save_txt_file(output_dir, f"classification_report_{model_file}", classification_repo)
+
+    print("Classification report saved successfully")
+
+
+if __name__ == "__main__":
+    main()
