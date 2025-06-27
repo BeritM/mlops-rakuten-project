626e39ebfd22c60092bbca1f616f0eb1d404024e
Sebastian Poppitz <sebastian.poppitz@haufe-akademie.de>
2025-06-27 09:52:12 +0200

    bug fix: admin auth

--- plugins/cd4ml/inference/auth_service.py
+++ plugins/cd4ml/inference/auth_service.py
@@ -32,7 +32,8 @@ def create_access_token(data: dict, expires_delta: timedelta = None):
     to_encode.update({"exp": expire})
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
-def verify_token(token: str):
+def verify_token(token: str = Depends(oauth2_scheme)):
+# def verify_token(token: str):
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         username = payload.get("sub")
